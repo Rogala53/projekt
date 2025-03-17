8756fed81f7b413f067c77406fb17bdd3f5fbb29
Rogala53 <lukrog4@gmail.com>
2025-03-17 17:48:18 +0100

    zad2

--- uczelnia.php
+++ uczelnia.php
@@ -7,24 +7,30 @@ if (!isset($_SESSION['account_1_balance'])) {
 }
 
 function addBalance(&$currentBalance, $valueToAdd) {
+    if($valueToAdd == null) return;
     $currentBalance += $valueToAdd;
+    $_SESSION['error_message'] = '';
 }
 
 
 function withdraw(&$currentBalance, $valueToWithdraw) {
+    if($valueToWithdraw == null) return;
     if ($currentBalance < $valueToWithdraw) {
-        echo "<script>alert('You don't have enough money to withdraw')</script>";
+        $_SESSION['error_message'] = 'You don\'t have enough money to withdraw';
     } else {
         $currentBalance -= $valueToWithdraw;
+        $_SESSION['error_message'] = '';
     }
 }
 
 function transfer(&$accountFrom, $valueToTransfer, &$accountTo) {
+    if($valueToTransfer == null) return;
     if ($accountFrom < $valueToTransfer) {
-        echo "<script>alert('You don't have enough money to transfer')</script>";
+        $_SESSION['error_message'] = 'You don\'t have enough money to transfer';
     } else {
         $accountFrom -= $valueToTransfer;
         $accountTo += $valueToTransfer;
+        $_SESSION['error_message'] = '';
     }
 }
 
@@ -65,25 +71,28 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <div id="container">
+        <div id="error" style="display: <?= !empty($_SESSION['error_message']) ? 'block' : 'none' ?>">
+            <?= $_SESSION['error_message'] ?>
+    </div>
         <div id="account1">
             <h2>Stan konta: <?= $_SESSION['account_1_balance'] ?> zł</h2>
             <form method="post">
-                <input type="number" name="valueToAdd1" min="1" value="valueToAdd1">
+                <input type="number" name="valueToAdd1" min="1" step="0.01" value="valueToAdd1">
                 <button type="submit" name="action" value="deposit1">Wpłać na konto</button>
-                <input type="number" name="valueToWithdraw1" min="0" value="valueToWithdraw1">
+                <input type="number" name="valueToWithdraw1" min="0" step="0.01" value="valueToWithdraw1">
                 <button type="submit" name="action" value="withdraw1">Wypłać z konta</button>
-                <input type="number" name="valueToTransfer1"min="1" value="valueToTransfer1">
+                <input type="number" name="valueToTransfer1"min="1" step="0.01" value="valueToTransfer1">
                 <button type="submit" name="action" value="transfer1">Przelew</button>
             </form>
         </div>
         <div id="account2">
             <h2>Stan konta: <?= $_SESSION['account_2_balance'] ?> zł</h2>
             <form method="post">
-                <input type="number" name="valueToAdd2" min="1" value="valueToAdd2">
+                <input type="number" name="valueToAdd2" min="1" step="0.01" value="valueToAdd2">
                 <button type="submit" name="action" value="deposit2">Wpłać na konto</button>
-                <input type="number" name="valueToWithdraw2" min="0" value="valueToWithdraw2">
+                <input type="number" name="valueToWithdraw2" min="1" step="0.01" value="valueToWithdraw2">
                 <button type="submit" name="action" value="withdraw2">Wypłać z konta</button>
-                <input type="number" name="valueToTransfer2" min="1" value="valueToTransfer2">
+                <input type="number" name="valueToTransfer2" min="1" step="0.01" value="valueToTransfer2">
                 <button type="submit" name="action" value="transfer2">Przelew</button>
             </form>
         </div>
@@ -97,6 +106,36 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         padding: 0;
         box-sizing: border-box;
     }
+    #error {
+        color: rgb(200, 32, 32);
+        background-color: rgb(243, 243, 243);
+        border: 2px solid rgb(200, 32, 32);
+        padding: 20px;
+        text-align: center;
+        font-weight: bolder;
+        position: absolute;
+        font-size: 16px;
+        top: 10px;
+        animation: slide 0.5s ease-out forwards, transparency 4s ease-out 1s forwards;
+        border-radius: 10px;
+        
+    }
+    @keyframes transparency {
+        0% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;           
+        }
+    }
+    @keyframes slide {
+        0% {
+            transform: translateY(-100px);
+        }
+        100% {
+            transform: translateY(0px);
+        }
+    }
     #container {
         display: flex;
         justify-content: space-around;
@@ -109,7 +148,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     h2 {
         font-size: 32px;
-        text-align: center;
     }
     form {
         margin-top: 20px;
