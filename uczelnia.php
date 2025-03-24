4beda7d7a5f8480b7e1aa281f7634149e0b4f802
Rogala53 <lukrog4@gmail.com>
2025-03-24 13:48:30 +0100

    Zad3

--- uczelnia.php
+++ uczelnia.php
@@ -4,62 +4,69 @@ session_start();
 if (!isset($_SESSION['account_1_balance'])) {
     $_SESSION['account_1_balance'] = 0;
     $_SESSION['account_2_balance'] = 0;
+    $_SESSION['form_token'] = bin2hex(random_bytes(32));
 }
 
-function addBalance(&$currentBalance, $valueToAdd) {
-    if($valueToAdd == null) return;
-    $currentBalance += $valueToAdd;
-    $_SESSION['error_message'] = '';
+function addBalance($account, $valueToAdd) {
+    $account += $valueToAdd;
+    $_SESSION['message_color'] = 'green';
+    $_SESSION['message'] = 'Adding balance successful';
+    return $account;
 }
 
-
-function withdraw(&$currentBalance, $valueToWithdraw) {
+function withdraw($account, $valueToWithdraw) {
     if($valueToWithdraw == null) return;
-    if ($currentBalance < $valueToWithdraw) {
-        $_SESSION['error_message'] = 'You don\'t have enough money to withdraw';
+    if($account < $valueToWithdraw) {
+        $_SESSION['message_color'] = 'red';
+        $_SESSION['message'] = 'You don\'t have enough money to withdraw';
     } else {
-        $currentBalance -= $valueToWithdraw;
-        $_SESSION['error_message'] = '';
+        $account -= $valueToWithdraw;
+        $_SESSION['message_color'] = 'green';
+        $_SESSION['message'] = 'Withdraw successful';
     }
+    return $account;
 }
 
-function transfer(&$accountFrom, $valueToTransfer, &$accountTo) {
+function transfer($accountFrom, $valueToTransfer, $accountTo) {
     if($valueToTransfer == null) return;
     if ($accountFrom < $valueToTransfer) {
-        $_SESSION['error_message'] = 'You don\'t have enough money to transfer';
+        $_SESSION['message_color'] = 'red';
+        $_SESSION['message'] = 'You don\'t have enough money to transfer';
     } else {
         $accountFrom -= $valueToTransfer;
         $accountTo += $valueToTransfer;
-        $_SESSION['error_message'] = '';
+        $_SESSION['message_color'] = 'green';
+        $_SESSION['message'] = 'Transfer successful';
     }
+    return array($accountFrom, $accountTo);
 }
 
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['action'])) {
+    if (isset($_POST['action']) && $_POST['form_token'] === $_SESSION['form_token']) {
         switch ($_POST['action']) {
             case 'deposit1':
-                addBalance($_SESSION['account_1_balance'], $_POST['valueToAdd1']);
+                $_SESSION['account_1_balance'] = addBalance($_SESSION['account_1_balance'], $_POST['valueToAdd1']);
                 break;
             case 'withdraw1':
-                withdraw($_SESSION['account_1_balance'], $_POST['valueToWithdraw1']);
+                $_SESSION['account_1_balance'] = withdraw($_SESSION['account_1_balance'], $_POST['valueToWithdraw1']);
                 break;
             case 'transfer1':
-                transfer($_SESSION['account_1_balance'], $_POST['valueToTransfer1'], $_SESSION['account_2_balance']);
+                list($_SESSION['account_1_balance'], $_SESSION['account_2_balance']) = transfer($_SESSION['account_1_balance'], $_POST['valueToTransfer1'], $_SESSION['account_2_balance']);
                 break;
             case 'deposit2':
-                addBalance($_SESSION['account_2_balance'], $_POST['valueToAdd2']);
+                $_SESSION['account_2_balance'] = addBalance($_SESSION['account_2_balance'], $_POST['valueToAdd2']);
                 break;
             case 'withdraw2':
-                withdraw($_SESSION['account_2_balance'], $_POST['valueToWithdraw2']);
+                $_SESSION['account_2_balance'] = withdraw($_SESSION['account_2_balance'], $_POST['valueToWithdraw2']);
                 break;
             case 'transfer2':
-                transfer($_SESSION['account_2_balance'], $_POST['valueToTransfer2'], $_SESSION['account_1_balance']);
+                list($_SESSION['account_2_balance'], $_SESSION['account_1_balance']) = transfer($_SESSION['account_2_balance'], $_POST['valueToTransfer2'], $_SESSION['account_1_balance']);
                 break;
         }
+        //Generowanie nowego tokenu
+        $_SESSION['form_token'] = bin2hex(random_bytes(32));
     }
-
-    header("Location: " . $_SERVER['PHP_SELF']);
-    exit();
 }
 ?>
 
@@ -67,107 +74,42 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Bank Account</title>
+    <title>Bank Accounts</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favico.png" type="image/x-icon">
 </head>
 <body>
     <div id="container">
-        <div id="error" style="display: <?= !empty($_SESSION['error_message']) ? 'block' : 'none' ?>">
-            <?= $_SESSION['error_message'] ?>
-    </div>
+        <div id="message" class="<?= $_SESSION['message_color']?>" style="display: <?= !empty($_SESSION['message']) ? 'block' : 'none'?>">
+            <?= $_SESSION['message'] ?>
+        </div>
         <div id="account1">
-            <h2>Stan konta: <?= $_SESSION['account_1_balance'] ?> zł</h2>
+            <img src="./images/PKO.png" alt="PKO" width="100px" height="122px">
+            <h2>Balance: <?= $_SESSION['account_1_balance'] ?> zł</h2>
             <form method="post">
-                <input type="number" name="valueToAdd1" min="1" step="0.01" value="valueToAdd1">
-                <button type="submit" name="action" value="deposit1">Wpłać na konto</button>
-                <input type="number" name="valueToWithdraw1" min="0" step="0.01" value="valueToWithdraw1">
-                <button type="submit" name="action" value="withdraw1">Wypłać z konta</button>
-                <input type="number" name="valueToTransfer1"min="1" step="0.01" value="valueToTransfer1">
-                <button type="submit" name="action" value="transfer1">Przelew</button>
+                <input type="hidden" name="form_token" value="<?= $_SESSION['form_token'] ?>">
+                <input type="number" name="valueToAdd1" min="1" step="0.01">
+                <button type="submit" name="action" value="deposit1">Add balance</button>
+                <input type="number" name="valueToWithdraw1" min="1" step="0.01">
+                <button type="submit" name="action" value="withdraw1">Withdraw</button>
+                <input type="number" name="valueToTransfer1"min="1" step="0.01">               
+                <button type="submit" name="action" value="transfer1">Transfer</button>
             </form>
         </div>
         <div id="account2">
-            <h2>Stan konta: <?= $_SESSION['account_2_balance'] ?> zł</h2>
+            <img src="./images/ING.png" alt="ING" width="100px">
+            <h2>Balance: <?= $_SESSION['account_2_balance'] ?> zł</h2>
             <form method="post">
-                <input type="number" name="valueToAdd2" min="1" step="0.01" value="valueToAdd2">
-                <button type="submit" name="action" value="deposit2">Wpłać na konto</button>
-                <input type="number" name="valueToWithdraw2" min="1" step="0.01" value="valueToWithdraw2">
-                <button type="submit" name="action" value="withdraw2">Wypłać z konta</button>
-                <input type="number" name="valueToTransfer2" min="1" step="0.01" value="valueToTransfer2">
-                <button type="submit" name="action" value="transfer2">Przelew</button>
+                <input type="hidden" name="form_token" value="<?= $_SESSION['form_token'] ?>">
+                <input type="number" name="valueToAdd2"min="1" step="0.01">
+                <button type="submit" name="action" value="deposit2">Add balance</button>
+                <input type="number" name="valueToWithdraw2" min="1" step="0.01">
+                <button type="submit" name="action" value="withdraw2">Withdraw</button>
+                <input type="number" name="valueToTransfer2"min="1" step="0.01">               
+                <button type="submit" name="action" value="transfer2">Transfer</button>
             </form>
         </div>
     </div>
 </body>
+<?= $_SESSION['message_color'] = ''?>
 </html>
-<style>
-    * {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    #error {
-        color: rgb(200, 32, 32);
-        background-color: rgb(243, 243, 243);
-        border: 2px solid rgb(200, 32, 32);
-        padding: 20px;
-        text-align: center;
-        font-weight: bolder;
-        position: absolute;
-        font-size: 16px;
-        top: 10px;
-        animation: slide 0.5s ease-out forwards, transparency 4s ease-out 1s forwards;
-        border-radius: 10px;
-        
-    }
-    @keyframes transparency {
-        0% {
-            opacity: 1;
-        }
-        100% {
-            opacity: 0;           
-        }
-    }
-    @keyframes slide {
-        0% {
-            transform: translateY(-100px);
-        }
-        100% {
-            transform: translateY(0px);
-        }
-    }
-    #container {
-        display: flex;
-        justify-content: space-around;
-        margin-top: 50px;
-    }
-    #account1, #account2 {
-        padding: 20px;
-        width: 300px;
-        text-align: center;    
-    }
-    h2 {
-        font-size: 32px;
-    }
-    form {
-        margin-top: 20px;
-    }
-    form > * {
-        margin: 10px;
-        padding: 15px;
-    }
-    form > input {
-        border: 1px solid black;
-        font-size: 16px;
-    }
-    button {
-        color: white;
-        cursor: pointer;
-        background-color:rgba(10, 0, 120, 0.73);
-        border: none;
-        border-radius:10px;
-    }
-    button:hover {
-        background-color: rgb(0, 0, 120);
-    }
-</style>
